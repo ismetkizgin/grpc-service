2a78d859354db9d7af6547c92e35a216ed4055b3
alihanakcm <alihanakcm@gmail.com>
2020-05-31 15:14:06 +0300

    feat: index files added

---
+++ index.js
@@ -0,0 +1,26 @@
+var PROTO_PATH = "./myfis.proto";
+
+var grpc = require("grpc");
+var protoLoader = require("@grpc/proto-loader");
+var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
+var hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
+
+function sayHello(call, callback) {
+  callback(null, { message: '{ "DEPREMİN FARKINA VAR"}' });
+}
+
+function main() {
+  var server = new grpc.Server();
+  server.addService(hello_proto.Greeter.service, { sayHello: sayHello });
+  server.bind("0.0.0.0:50051", grpc.ServerCredentials.createInsecure());
+  server.start();
+  console.log("grpc service is live : " + "0.0.0.0:50051");
+}
+
+main();
