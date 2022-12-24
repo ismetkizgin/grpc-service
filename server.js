406c3cdb1b46c16d0c885057a0f70a07153ab55d
ismet kizgin <info@ismetkizgin.com.tr>
2022-12-24 19:33:00 +0300

    feat: update grpc server example

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-var PROTO_PATH = "./myfis.proto";
-
 var grpc = require("grpc");
 var protoLoader = require("@grpc/proto-loader");
-var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+
+var packageDefinition = protoLoader.loadSync("./hello.proto", {
   keepCase: true,
   longs: String,
   enums: String,
   defaults: true,
   oneofs: true,
 });
+
 var hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
 
 function sayHello(call, callback) {
   console.log(`request parameters: ${call.request.name}`);
-  callback(null, { message: `User Name: ${call.request.name}` });
+  callback(null, { message: `Hello ${call.request.name}` });
 }
 
 function main() {
   var server = new grpc.Server();
-  server.addService(hello_proto.Greeter.service, { sayHello: sayHello });
+  server.addService(hello_proto.Hello.service, { sayHello: sayHello });
   server.bind("0.0.0.0:50051", grpc.ServerCredentials.createInsecure());
   server.start();
   console.log("grpc service is live : " + "0.0.0.0:50051");
